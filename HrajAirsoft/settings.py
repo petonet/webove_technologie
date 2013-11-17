c9a659b08dd62720ae3fd099485004020f64c628
Matus Husovsky <mahu358@gmail.com>
2013-11-17 19:49:53 +0100

    ##########Changelog################ --hladanie chyby v tagu teams
    #########TODO####################
    -vsetko

--- HrajAirsoft/settings.py
+++ HrajAirsoft/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
-}
+} 
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
