5d5c527529e6729d455c768fcb8d5ed967e108d3
Matus Husovsky <mahu358@gmail.com>
2013-11-26 21:30:06 +0100

    ##########Changelog################ -este len fakt ze zakladne prihlasenie/odhlasenie bez nicoho dlasieho
    #########TODO####################
    -vsetko

--- players/forms.py
+++ players/forms.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from players.models import player
+from django.contrib.auth.models import User
 from django import forms
 
 class PlayerForm(forms.ModelForm):
-    username = forms.CharField(label='Užívateľské meno', widget=forms.TextInput, required=True)
+   #username = forms.CharField(label='Užívateľské meno', widget=forms.TextInput, required=True)
     password1 = forms.CharField(label='Heslo', widget=forms.PasswordInput, required=True)
     password2 = forms.CharField(label='Heslo znovu', widget=forms.PasswordInput, required=True)
     first_name = forms.CharField(label='Meno', widget=forms.TextInput, required=False)
@@ -18,7 +19,8 @@ class PlayerForm(forms.ModelForm):
 
     class Meta:
         model = player
-        exclude = ('last_login','is_superuser', 'is_staff', 'is_active','date_joined')
+        model = User
+        exclude = ('last_login','is_superuser', 'is_staff', 'is_active','date_joined','username','password','groups','user_permissions')
 
     def clean_password2(self):
         if self.cleaned_data.get('password1') != self.cleaned_data['password2']:
--- players/urls.py
+++ players/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+import HrajAirsoft
 
 
 urlpatterns = patterns('',
    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'players/login.html'}),
-   url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'players/logout.html'}),
+   #url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'players/logout.html'}),
+   url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'homepage/homepage.html'}),
    url(r'^profile/$', 'players.views.profile'),
    )
